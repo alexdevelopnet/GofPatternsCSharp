af4008621bba4ac8e9cbcd57cb6c0a621d4bc427
AlexFeitoza <alexdevelopnet@gmail.com>
2025-08-02 09:34:44 -0300

    organizando

--- obj/Debug/net9.0/GoFPatternsCSharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/GoFPatternsCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoFPatternsCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e435e000c3164aab2c71ebdf37885ab2769399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e65bfe0b92fff948d393064b8fe25fff6e6285")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoFPatternsCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoFPatternsCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
